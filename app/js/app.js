551d7a6c65099bfef2d926e33d06802083d52f4a
pratibha <pratibhashree21@gmail.com>
2020-11-07 10:03:15 +0530

    initial changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,10 @@
 angular.module('RecipeManagement', [
-  'RecipeManagement.services',
-  'RecipeManagement.controllers',
-  'ngRoute'
+    'RecipeManagement.services',
+    'RecipeManagement.controllers',
+    'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  when("/recipe", {templateUrl: "partials/recipe.html", controller: "recipeController"}).
-  when("/recipeBackup", {templateUrl: "partials/recipeBackup.html", controller: "recipeBackupController"}).
-
-	when("/shopping", {templateUrl: "partials/shopping.html", controller: "shoppingController"}).
-	otherwise({redirectTo: '/recipe'});
+    $routeProvider.
+    when("/recipe", { templateUrl: "partials/recipe.html", controller: "recipeController" }).
+    otherwise({ redirectTo: '/recipe' });
 }]);
